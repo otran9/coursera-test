6dc6ce5648dbba054660aac8e5dd69f14842769d
otran9 <36254277+otran9@users.noreply.github.com>
2021-02-22 14:04:45 -0500

    fixed undefined names

--- module4-solution/script.js
+++ module4-solution/script.js
@@ -28,8 +28,7 @@ assignment.
 WARNING!!! WARNING!!!
 
 */
-helloSpeaker.speak();
-byeSpeaker.speak();
+
 // STEP 1:
 // Wrap the entire contents of script.js inside of an IIFE
 // See Lecture 52, part 2
